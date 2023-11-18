37be8dd302e93cde59ff5ed5cd8fee9641f235d5
Hello <deftqml@gmail.com>
2023-11-18 20:25:12 +0700

    Fix loading skeleton key issue

--- app/predict/loading.tsx
+++ app/predict/loading.tsx
@@ -9,8 +9,8 @@ const Loading = () => {
                 <div className="h-20 rounded-lg bg-default-300"></div>
             </Skeleton>
 
-            {Array.from({ length: 3 }, (_, i) => i + 1).map(() => (
-                <Skeleton className="w-full h-10 rounded-md" />
+            {Array.from({ length: 3 }, (_, i) => i + 1).map((i) => (
+                <Skeleton key={i} className="w-full h-10 rounded-md" />
             ))}
         </div>
     )
